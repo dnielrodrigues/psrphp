aa3a777b3b346cc8dfed179309b550d3cfa25340
Daniel Rodrigues <danielrodrigues.a113@gmail.com>
2021-01-22 15:10:32 -0300

    [feat] ajustes em rotas

--- config/config.php
+++ config/config.php
@@ -1,5 +1,8 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 // debug mode
 ini_set('display_errors', true);
 ini_set('error_reporting', E_ALL);
@@ -10,3 +13,16 @@ define('BASE_PATH', dirname(__DIR__));
 define('SITE_PATH', BASE_PATH.'/public');
 define('THEME_PATH', SITE_PATH.'/themes/my-theme');
 define('THEME_URL', BASE_URL.'/themes/my-theme');
+
+// route config
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
+$responseFactory = new \Laminas\Diactoros\ResponseFactory();
+$strategy = new League\Route\Strategy\JsonStrategy($responseFactory);
+$router   = (new League\Route\Router)->setStrategy($strategy);
+
+// route map
+require_once BASE_PATH . '/routes.php';
+
+// dispatch route
+$response = $router->dispatch($request);
+(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
--- routes.php
+++ routes.php
@@ -1,30 +1,16 @@
 <?php
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 
-// route config
-$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
-$responseFactory = new \Laminas\Diactoros\ResponseFactory();
-$strategy = new League\Route\Strategy\JsonStrategy($responseFactory);
-$router   = (new League\Route\Router)->setStrategy($strategy);
-
-// map route
 $router->get('/', 'App\Ctrls\SiteCtrl::home');
-
 $router->group('/api', function (\League\Route\RouteGroup $route) {
   $route->get('/', 'App\Ctrls\ApiCtrl::home');
   foreach ([
     'usuarios' => 'App\Ctrls\UsuarioCtrl',
     'pessoas' => 'App\Ctrls\PessoaCtrl',
   ] as $slug => $ctrl) {
-    $route->get("/$slug", $ctrl.'::list');
-    $route->get("/$slug/{id}", $ctrl.'::get');
-    $route->post("/$slug", $ctrl.'::save');
-    $route->post("/$slug/{id}", $ctrl.'::save');
-    $route->delete("/$slug/{id}", $ctrl.'::delete');
+    $route->get("/$slug", "$ctrl::list");
+    $route->get("/$slug/{id}", "$ctrl::get");
+    $route->post("/$slug", "$ctrl::save");
+    $route->post("/$slug/{id}", "$ctrl::save");
+    $route->delete("/$slug/{id}", "$ctrl::delete");
   }
 });
-
-// end route
-$response = $router->dispatch($request);
-(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
